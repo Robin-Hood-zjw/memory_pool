317e74a53431d3050510b8e4fa9e3b7de9e3f13a
Robin-Hood-zjw <jiawenz@umich.edu>
2026-02-19 15:02:19 -0800

    0.1.2-initialzing_MemoryPool_class

--- src/v1/memory_pool.h
+++ src/v1/memory_pool.h
@@ -1,8 +1,30 @@
 #pragma once
 
+#include <mutex>
 #include <atomic>
 
 struct Slot {
     std::atomic<Slot*> next;
 };
 
+class MemoryPool {
+private:
+    int _slotSize;
+    int _blockSize;
+    std::mutex _mutex;
+
+    Slot* _curSlot;
+    Slot* _lastSlot;
+    std::atomic<Slot*> _freeSlot;
+
+    void allocateNewBlock();
+
+    size_t padPointer(char* p, size_t align);
+
+    bool pushFreeList(Slot* slot);
+
+    Slot* popFreeList();
+public:
+    MemoryPool(size_t blockSize = 4096);
+    ~MemoryPool();
+};
