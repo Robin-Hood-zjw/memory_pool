866b5c5c27530db03110fe247e83db72ef0a339c
Robin-Hood-zjw <jiawenz@umich.edu>
2026-02-20 22:25:27 -0800

    0.1.14-implementing_HashBucket_methods

---
+++ src/v1/HashBucket.cpp
@@ -0,0 +1,33 @@
+#pragma once
+
+#include "./HashBucket.h"
+
+namespace MemoryPool {
+    void HashBucket::initMemoryPool() {
+        for (int i = 0; i < MEMORY_POOL_NUM; i++) {
+            getMemoryPool(i).init((i + 1) * SLOT_BASE_SIZE);
+        }
+    }
+
+    MemoryPool& HashBucket::getMemoryPool(int index) {
+        static MemoryPool memoryPool[MEMORY_POOL_NUM];
+        return memoryPool[index];
+    }
+
+    void* HashBucket::useMemory(size_t size) {
+        if (size <= 0) return nullptr;
+        if (size > MAX_SLOT_SIZE) return operator new(size);
+
+        return getMemoryPool((size + 7) / SLOT_BASE_SIZE - 1).allocate();
+    }
+
+    void HashBucket::freeMemory(void* ptr, size_t size) {
+        if (!ptr) return;
+        if (size > MAX_SLOT_SIZE) {
+            operator delete(ptr);
+            return;
+        }
+
+        getMemoryPool((size + 7) / SLOT_BASE_SIZE - 1).deallocate(ptr);
+    }
+};
--- src/v1/HashBucket.h
+++ src/v1/HashBucket.h
@@ -12,22 +12,8 @@ namespace MemoryPool {
             static void initMemoryPool();
             static MemoryPool& getMemoryPool(int index);
 
-            static void* useMemory(size_t size) {
-                if (size <= 0) return nullptr;
-                if (size > MAX_SLOT_SIZE) return operator new(size);
-
-                return getMemoryPool((size + 7) / SLOT_BASE_SIZE - 1).allocate();
-            }
-
-            static void freeMemory(void* ptr, size_t size) {
-                if (!ptr) return;
-                if (size > MAX_SLOT_SIZE) {
-                    operator delete(ptr);
-                    return;
-                }
-
-                getMemoryPool((size + 7) / SLOT_BASE_SIZE - 1).deallocate(ptr);
-            }
+            static void* useMemory(size_t size);
+            static void freeMemory(void* ptr, size_t size);
 
             template<typename T, typename... Args>
             friend T* newElement(Args&&... args);
