4077315fff552704436fb33fe8f715c4f14382e7
Robin-Hood-zjw <jiawenz@umich.edu>
2026-02-22 13:00:57 -0800

    0.2.1-initializing_Size_Class

---
+++ v2/include/Common.h
@@ -0,0 +1,27 @@
+#pragma once
+
+#include <array>
+#include <atomic>
+
+namespace Pool {
+    constexpr size_t ALIGNMENT = 8;
+    constexpr size_t MAX_BYTES = 256 * 1024;
+    constexpr size_t FREE_LIST_SIZE = MAX_BYTES / ALIGNMENT;
+
+    struct BlockHeader {
+        bool inUse;
+        size_t size;
+        BlockHeader* next;
+    };
+
+    class SizeClass {
+        static size_t roundUp(size_t bytes) {
+            return (bytes + ALIGNMENT - 1) / ALIGNMENT * ALIGNMENT;
+        }
+
+        static size_t getIndex(size_t bytes) {
+            bytes = std::max(bytes, ALIGNMENT);
+            return (bytes + ALIGNMENT - 1) / ALIGNMENT - 1;
+        }
+    };
+}
